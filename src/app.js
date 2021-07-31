076e4c23e7e6d96a1cbb9ceb58d4c120342acf01
santosh <santosh.ece06@gmail.com>
2021-07-31 11:53:30 +0530

    blank-user-list

--- src/app.js
+++ src/app.js
@@ -1,35 +1,9 @@
 const express = require("express");
 const app = express();
 
-// http://localhost:4000/
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
-// http://localhost:4000/1
-app.get("/1", (req, res) => {
-  let ref = { id: 1, name: "rahul" };
-  res.json(ref);
-});
-
-// http://localhost:4000/2
-app.get("/2", (req, res) => {
-  let list = ["delhi", "calcutta", "mumbai"];
+app.get("/user-list", (req, res) => {
+  let list = [];
   res.json(list);
 });
 
-// http://localhost:4000/3
-app.get("/3", (req, res) => {
-  let list = [
-    { id: 1, name: "rahul", email: "rahul@gmail.com", mobile: "2323233" },
-    { id: 2, name: "rohit", email: "rohit@gmail.com", mobile: "23232323" },
-    { id: 1, name: "rahul", email: "rahul@gmail.com", mobile: "2323233" },
-    { id: 2, name: "rohit", email: "rohit@gmail.com", mobile: "23232323" },
-    { id: 1, name: "rahul", email: "rahul@gmail.com", mobile: "2323233" },
-    { id: 2, name: "rohit", email: "rohit@gmail.com", mobile: "23232323" },
-  ];
-
-  res.json(list);
-});
-
-app.listen(4000, () => console.log("Server started"));
+app.listen(4000, () => console.log("server starteddd...."));
