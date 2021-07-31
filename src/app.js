663531a515b57243cd87f06b4b0b6ba0e9ba380e
santosh <santosh.ece06@gmail.com>
2021-07-31 11:38:33 +0530

    b1

--- src/app.js
+++ src/app.js
@@ -1,4 +1,10 @@
 const express = require("express");
 const app = express();
 
+// app.get();
+// app.get("/", () => {});
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
 app.listen(4000, () => console.log("Server started"));
