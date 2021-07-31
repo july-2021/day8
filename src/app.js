56dc4f4b1fe056e38211d33c0ab772bf90554aa7
santosh <santosh.ece06@gmail.com>
2021-07-31 12:32:14 +0530

    static user usig the post api.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,24 @@
+const mysql = require("mysql");
+const Promise = require("bluebird");
+Promise.promisifyAll(require("mysql/lib/Connection").prototype);
+Promise.promisifyAll(require("mysql/lib/Pool").prototype);
+
 const express = require("express");
 const app = express();
 
-// http://localhost:4000/user-create
-app.post("/user-create", (req, res) => {
+const dbconfig = { host: "localhost", user: "root", password: "mysql" };
+
+app.post("/user-create", async (req, res) => {
+  const connection = mysql.createConnection(dbconfig);
+  await connection.connectAsync();
+
+  const sql = `insert into edac.user (username, password, email, mobile) values (?, ?, ?, ?)`;
+  const params = ["aditya", "#@#@##", "aditya@gmail.com", "2323232"];
+
+  await connection.queryAsync(sql, params);
+
+  await connection.endAsync();
+
   res.json({ message: "User created Successfully" });
 });
 
